76fdb25312e017bd91e40847b7a40b4739ac9176
Hyebin <thsuekfk2@naver.com>
2022-01-26 16:42:45 +0900

    로그인 기능 Bcrypt & jsonwebtoken으로 토큰생성

--- models/User.js
+++ models/User.js
@@ -1,7 +1,7 @@
 const mongoose = require("mongoose");
 const bcrypt = require("bcrypt");
 const saltRounds = 10;
-
+const jwt = require("jsonwebtoken");
 const userSchema = mongoose.Schema({
   name: {
     type: String,
@@ -14,7 +14,7 @@ const userSchema = mongoose.Schema({
   },
   password: {
     type: String,
-    maxlength: 10,
+    maxlength: 100,
   },
   lastname: {
     type: String,
@@ -57,6 +57,26 @@ userSchema.pre("save", function (next) {
     next();
   }
 });
+
+userSchema.methods.comparePassword = function (plainPassword, cb) {
+  //plainPassword 1234567 암호화된 비밀번호 :
+  bcrypt.compare(plainPassword, this.password, function (err, isMatch) {
+    if (err) return cb(err);
+    cb(null, isMatch);
+  });
+};
+
+userSchema.methods.generateToken = function (cb) {
+  var user = this;
+  //jsonwebtoken을 이용해서 token 을 생성하기
+  var token = jwt.sign(user._id.toHexString(), "secreatToken");
+
+  user.token = token;
+  user.save(function (err, user) {
+    if (err) return cb(err);
+    cb(null, user);
+  });
+};
 
 const User = mongoose.model("User", userSchema);
 module.exports = { User };
--- server.js
+++ server.js
@@ -5,11 +5,14 @@ const mongoose = require("mongoose");
 const bodyParser = require("body-parser");
 const { User } = require("./models/User");
 const config = require("./config/key");
+const cookieParser = require("cookie-parser");
+
 //클라이언트에서 오는 정보를 서버에서 분석해서 가져올 수 있다.
 app.use(bodyParser.urlencoded({ extended: true }));
 
 //json 타입으로 된 것을 분석해서 가져올 수 있다.
 app.use(bodyParser.json());
+app.use(cookieParser());
 
 mongoose
   .connect(config.mongoURI, { useNewUrlParser: true, useUnifiedTopology: true })
@@ -30,5 +33,37 @@ app.post("/register", (req, res) => {
     });
   });
 });
+app.post("/login", (req, res) => {
+  //요청된 이메일을 데이터베이스에서 있는지 찾는다
+  User.findOne({ email: req.body.email }, (err, user) => {
+    if (!user) {
+      return res.json({
+        loginSuccess: false,
+        message: "제공된 이메일에 해당하는 유저가 없습니다.",
+      });
+    }
+
+    //요청된 이메일이 데이터 베이스에 있다면 비밀번호 인지 확인
+    user.comparePassword(req.body.password, (err, isMatch) => {
+      if (!isMatch)
+        return res.json({
+          loginSuccess: false,
+          message: "비밀번호가 틀렸습니다.",
+        });
+
+      //비밀번호가 맞다면 token 생성
+      user.generateToken((err, user) => {
+        if (err) return res.status(400).send(err);
+
+        //token을 저장한다. 어디에? 쿠키 , 로컬스토리지, 세션 - > 나는 쿠키에 저장
+        res
+          .cookie("x_auth", user.token)
+          .status(200)
+          .json({ loginSuccess: true, userId: user._id });
+      });
+    });
+  });
+});
+
 const port = 5000;
 app.listen(port, () => console.log(`${port}`));
