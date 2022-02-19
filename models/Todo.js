bf4cb8eb9b74549ec8aa78a037212e895caf3a7d
Hyebin <thsuekfk2@naver.com>
2022-02-19 19:57:32 +0900

    전달받은 todo 정보를 데이터베이스에 저장하기

---
+++ models/Todo.js
@@ -0,0 +1,40 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+const todoSchema = mongoose.Schema(
+  {
+    writer: {
+      type: Schema.Types.ObjectId,
+      ref: "User",
+    },
+    title: {
+      type: String,
+      maxlength: 50,
+    },
+    description: {
+      type: String,
+    },
+    price: {
+      type: Number,
+      default: 0,
+    },
+    images: {
+      type: Array,
+      default: [],
+    },
+    sold: {
+      type: Number,
+      maxlength: 100,
+      default: 0,
+    },
+    views: {
+      type: Number,
+      default: 0,
+    },
+  },
+  { timestamps: true }
+  //자동으로 등록시간 업데이트가 기록됨
+);
+
+const Todo = mongoose.model("Todo", todoSchema);
+module.exports = { Todo };
--- routes/todo.js
+++ routes/todo.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const router = express.Router();
 const multer = require("multer");
+const { Todo } = require("../models/Todo");
 //================================
 //             Todo
 //================================
@@ -31,6 +32,15 @@ router.post("/image", (req, res) => {
       filePath: res.req.file.path,
       fileName: res.req.file.filename,
     });
+  });
+});
+
+router.post("/", (req, res) => {
+  //받아온 정보들을 db에 넣어준다.
+  const todo = new Todo(req.body);
+  todo.save((err) => {
+    if (err) return res.status(400).json({ success: false, err });
+    return res.status(200).json({ success: true });
   });
 });
 
